5a62ae440130943c1fb37cd4c76f3122fc4fb4c1
Ross Jones <ross@servercode.co.uk>
2016-07-28 19:24:02 +0100

    Fix the test that looks for text on homepage

--- src/dguweb/test/controllers/page_controller_test.exs
+++ src/dguweb/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule DGUWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "published by government departments"
   end
 end
