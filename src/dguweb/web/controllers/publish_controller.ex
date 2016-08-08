af66fb00f26f99fdd367e8ccd8dedf6a408b8f03
Ross Jones <ross.jones@digital.cabinet-office.gov.uk>
2016-08-08 14:54:02 +0100

    Simplify to a single upload/url

--- src/dguweb/web/controllers/publish_controller.ex
+++ src/dguweb/web/controllers/publish_controller.ex
@@ -5,34 +5,59 @@ defmodule DGUWeb.PublishController do
   def index(conn, _params) do
     conn
     |> clear_session
-    |> render("index.html", current_files: [])
+    |> render("index.html", current_files: [], error: :nil)
   end
 
-  def add_file(conn, %{"file"=>upload}=params) do
+  ### Add File, upload or URL ################################################
+  #
+  # When (if) we decide that this is better tracked in the database, we can 
+  # rely on the changeset to handle the difference validation requirements 
+  # and/or we can change the process flow.
+  #
+  ############################################################################
+
+  def add_file(conn, params) do
+    file = Map.get(params, "file", :nil)
+    url = Map.get(params, "url", :nil)
+
+    add_file_internal(conn, {file, url})
+  end 
+
+  defp add_file_internal(conn, {:nil, :nil}), do: failed_add_file(conn, "Please supply a URL or a file upload.")
+  defp add_file_internal(conn, {upload, ""}) do 
     # We have to move this file before the request completes
     info = %UploadInfo{filename: upload.filename}
+    progress_add_file(conn, info)
+  end 
 
-    render_add_file(conn, info)
+  defp add_file_internal(conn, {:nil, "http" <> url}) do 
+    info = %UploadInfo{filename: "http" <> url}
+    progress_add_file(conn, info)
+  end 
+
+  defp add_file_internal(conn, {:nil, _url}), do: failed_add_file(conn, "URL does not appear to be valid")
+  defp add_file_internal(conn, {_, _}), do: failed_add_file(conn, "Please supply a URL or a file upload, not both")
+
+  defp failed_add_file(conn, error) do 
+    conn
+    |> render("index.html", current_files: [], error: error)    
   end
 
-  def add_file(conn, %{"url"=>url}=params) do
-    info = %UploadInfo{filename: url}    
-    
-    render_add_file(conn,  info)
-  end
-
-  defp render_add_file(conn, info) do
+  defp progress_add_file(conn, info) do
     current = [info | get_session(conn, :current_files) || []]
 
     conn
     |> put_session(:current_files, current)
-    |> render("index.html", current_files: current)
-end 
+    |> redirect(to: publish_path(conn, :find, []))
+  end 
+
+  ### Determine what the user wants to do with the uploaded data #############
 
   def find(conn, _params) do 
     current = get_session(conn, :current_files) || []
 
     render(conn, "find.html", current_files: current)
   end 
+
 
 end
