6fd8cd43e76137f5fbdefc7ebe9a1aeadc8979ea
Ross Jones <ross@servercode.co.uk>
2016-08-14 17:39:33 +0100

    Some changes to the publisher index+show pages
    Remove some stuff from the publisher pages and add temporary auth so we
    can model the user being able to add data (or not) to a particular
    publisher.
    
    Makes assumptions that the publisher workflow can take either a
    publisher or a dataset (in which case the workflow is really short).

--- src/dguweb/web/controllers/publish_controller.ex
+++ src/dguweb/web/controllers/publish_controller.ex
@@ -6,14 +6,13 @@ defmodule DGUWeb.PublishController do
 
   def index(conn, _params) do
     conn
-    |> clear_session
     |> render("index.html", current_files: [], error: :nil)
   end
 
   ### Add File, upload or URL ################################################
   #
-  # When (if) we decide that this is better tracked in the database, we can 
-  # rely on the changeset to handle the difference validation requirements 
+  # When (if) we decide that this is better tracked in the database, we can
+  # rely on the changeset to handle the difference validation requirements
   # and/or we can change the process flow.
   #
   ############################################################################
@@ -23,32 +22,32 @@ defmodule DGUWeb.PublishController do
     url = Map.get(params, "url", :nil)
 
     add_file_internal(conn, {file, url})
-  end 
+  end
 
   defp add_file_internal(conn, {:nil, :nil}), do: failed_add_file(conn, "Please supply a URL or a file upload.")
-  defp add_file_internal(conn, {upload, ""}) do 
+  defp add_file_internal(conn, {upload, ""}) do
     info = UploadInfo.from_upload(upload)
     spawn(fn-> Tasks.run_tasks(info, Tasks.file_tasks) end )
     progress_add_file(conn, info)
-  end 
+  end
 
-  defp add_file_internal(conn, {:nil, "http" <> url}) do 
+  defp add_file_internal(conn, {:nil, "http" <> url}) do
     info = UploadInfo.from_url("http" <> url)
     |>  Tasks.run_tasks(Tasks.url_tasks)
 
-    case info.errors do 
+    case info.errors do
       [] -> progress_add_file(conn, info)
       [x] -> failed_add_file(conn, x)
-    end 
-    
-  end 
+    end
+
+  end
 
   defp add_file_internal(conn, {:nil, _url}), do: failed_add_file(conn, "URL does not appear to be valid")
   defp add_file_internal(conn, {_, _}), do: failed_add_file(conn, "Please supply a URL or a file upload, not both")
 
-  defp failed_add_file(conn, error) do 
+  defp failed_add_file(conn, error) do
     conn
-    |> render("index.html", current_files: [], error: error)    
+    |> render("index.html", current_files: [], error: error)
   end
 
   defp progress_add_file(conn, info) do
@@ -57,15 +56,15 @@ defmodule DGUWeb.PublishController do
     conn
     |> put_session(:current_files, current)
     |> redirect(to: publish_path(conn, :find, []))
-  end 
+  end
 
   ### Determine what the user wants to do with the uploaded data #############
 
-  def find(conn, _params) do 
+  def find(conn, _params) do
     current = get_session(conn, :current_files) || []
 
     render(conn, "find.html", current_files: current)
-  end 
+  end
 
 
 end
