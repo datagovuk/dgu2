0b7ff3ba2262d8f74f48b3edb80d1d32d22bc438
Max Froumentin <max@froumentin.net>
2016-08-16 11:12:15 +0100

    Rebase master and de-conflict

--- src/dguweb/web/static/elm/App.elm
+++ src/dguweb/web/static/elm/App.elm
@@ -1,6 +1,130 @@
 
 module App exposing (..)
 import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (..)
+import Task
 
+
+main: Program Never
 main =
-  Html.text ""
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
+
+-- MODEL
+
+type alias Dataset =
+  { title: String
+  , publisher_title: String
+  , name: String
+  }
+
+
+type alias Model = List Dataset
+
+
+init : (Model, Cmd Msg)
+init =
+  ([], Cmd.none)
+
+
+-- UPDATE
+
+
+type Msg
+  = Lookup String
+  | FetchSucceed Model
+  | FetchFail Http.Error
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Lookup q ->
+      (model, getMatchingDatasets q)
+
+    FetchSucceed resultList ->
+      (resultList, Cmd.none)
+
+    FetchFail error ->
+      (model, Cmd.none)
+
+
+getMatchingDatasets: String -> Cmd Msg
+getMatchingDatasets query =
+  let
+    url = "/search?_format=json&q=" ++ query
+  in
+    Task.perform
+      FetchFail
+      FetchSucceed
+      (Http.get searchApiDecoder url)
+
+
+searchApiDecoder: Decoder Model
+searchApiDecoder =
+  Json.Decode.list
+    (object3 Dataset
+      ("title" := Json.Decode.string)
+      ("publisher_title" := Json.Decode.string)
+      ("name" := Json.Decode.string)
+    )
+
+
+-- VIEW
+
+
+viewCompletionItem : Dataset -> Html Msg
+viewCompletionItem result =
+  Html.li []
+    [ Html.a [ href ("/dataset/" ++ result.name) ]
+        [ Html.text (result.title ++ " - " ++ result.publisher_title) ]
+    ]
+
+
+viewCompletionMenu : Model -> Html Msg
+viewCompletionMenu results =
+  Html.div [ class "autocomplete-menu" ]
+    [ Html.ul []
+      (List.map viewCompletionItem results)
+    ]
+
+
+viewForm : Html Msg
+viewForm =
+  Html.form
+    [ action "/search"
+    , method "GET"
+    ]
+    [ Html.fieldset []
+      [ Html.input
+        [ id "q"
+        , name "q"
+        , type' "text"
+        , placeholder "Search for data"
+        , class "form-control search"
+        , autocomplete False
+        , onInput Lookup
+        ]
+        []
+      ]
+    ]
+
+
+view : Model -> Html Msg
+view model =
+  Html.div []
+    (if (List.length model > 0) then
+      [ viewForm
+      , viewCompletionMenu model
+      ]
+    else
+      [ viewForm ]
+    )
